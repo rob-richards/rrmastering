da35d3e2d220bfb3a1482e570e748256fd0524b2
Rob Richards <rob@robrichards.info>
2022-11-24 20:50:15 -0600

    updates

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -29,15 +29,15 @@ export default function Uses() {
   return (
     <>
       <Head>
-        <title>Uses - Spencer Sharp</title>
+        <title>Uses - Rob Richards Mastering</title>
         <meta
           name="description"
-          content="Software I use, gadgets I love, and other things I recommend."
+          content="Software I use, gear I love, and other things I recommend."
         />
       </Head>
       <SimpleLayout
-        title="Software I use, gadgets I love, and other things I recommend."
-        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+        title="Software I use, gear I love, and other things I recommend."
+        intro="I get asked a lot about the things I use when mastering, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
       >
         <div className="space-y-20">
           <ToolsSection title="Workstation">
