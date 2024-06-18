7bcf97ddbdb336bf9019af3b1dadfb381b932154
Rob Richards <rob@robrichards.info>
2024-06-18 16:30:11 -0500

    updates

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Document() {
   return (
@@ -20,6 +21,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
   );
