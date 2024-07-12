0729144a0b7cfc73a873ad468307610ada3661e1
Rob Richards <rob@robrichards.info>
2024-07-12 16:13:11 -0500

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ export function Footer() {
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 {/* <NavLink href="/blog">Blog</NavLink> */}
-                <NavLink href="/cheat-sheet">Cheat Sheet</NavLink>
+                <NavLink href="/cheatsheet">Cheat Sheet</NavLink>
                 <NavLink href="/book">Book a Session</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -91,7 +91,7 @@ function MobileNavigation(props) {
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/about">About</MobileNavItem>
                 {/* <MobileNavItem href="/blog">Blog</MobileNavItem> */}
-                <MobileNavItem href="/cheat-sheet">Cheat Sheet</MobileNavItem>
+                <MobileNavItem href="/cheatsheet">Cheat Sheet</MobileNavItem>
                 <MobileNavItem href="/book">Book a Session</MobileNavItem>
               </ul>
             </nav>
@@ -131,7 +131,7 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
         {/* <NavItem href="/blog">Blog</NavItem> */}
-        <NavItem href="/cheat-sheet">Cheat Sheet</NavItem>
+        <NavItem href="/cheatsheet">Cheat Sheet</NavItem>
         <NavItem href="/book">Book a Session</NavItem>
       </ul>
     </nav>
