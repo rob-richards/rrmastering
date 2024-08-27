c5abc28839ea5d2cfe51e13b1450eaa996d672ac
Rob Richards <rob@robrichards.info>
2024-08-27 12:36:05 -0500

    updates

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -44,7 +44,7 @@ export default function ArticlesIndex({ articles }) {
       </Head>
       <SimpleLayout
         title="Writing on music production, software design, and the intersection of both."
-        intro="Thoughts and Insights on Producing, Mixing, Masting, and more."
+        intro="Thoughts and Insights on Producing, Mixing, Mastering, and more."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
