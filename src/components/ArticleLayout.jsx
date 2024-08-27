960f1adfff2a07af7e7088c26caa00ce1b3f7ef6
Rob Richards <rob@robrichards.info>
2024-08-27 11:01:33 -0500

    update blog

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -38,7 +38,7 @@ export function ArticleLayout({
       </Head>
       <Container className="mt-16 lg:mt-32">
         <div className="xl:relative">
-          <div className="mx-auto max-w-2xl">
+          <div className="mx-auto max-w-3xl">
             {previousPathname && (
               <button
                 type="button"
@@ -51,16 +51,9 @@ export function ArticleLayout({
             )}
             <article>
               <header className="flex flex-col">
-                <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                <h1 className="mt-0 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
                   {meta.title}
                 </h1>
-                <time
-                  dateTime={meta.date}
-                  className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
-                >
-                  <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-                  <span className="ml-3">{formatDate(meta.date)}</span>
-                </time>
               </header>
               <Prose className="mt-8">{children}</Prose>
             </article>
