3b8918a4d680ce0d0e29c2cdf2d710911a8be150
Rob Richards <rob@wavedivision.audio>
2025-06-02 14:56:51 -0500

    ga updates

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,21 @@ const nextConfig = {
     newNextLinkBehavior: true,
     scrollRestoration: true,
   },
+  // Add Google Analytics configuration
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; object-src 'none'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withMDX = nextMDX({
